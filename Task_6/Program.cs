bc48ce1dd0d26a4a23719339ad2ac996a04dd960
Yana <leduschenkova6@gmail.com>
2022-09-17 18:14:17 +0300

    add Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 6. Вводим число и узнаем четное оно или нет
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0)
+{
+    Console.Write("это число четное");
+}
+else
+    Console.Write("это число нечетное");
