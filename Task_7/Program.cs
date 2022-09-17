365fd63103c5d74b44eb5833054a0d89e6a76a3e
Yana <leduschenkova6@gmail.com>
2022-09-17 18:14:30 +0300

    add Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,5 @@
+﻿// на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Write("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = (num%100)%10;
+Console.Write(result);
