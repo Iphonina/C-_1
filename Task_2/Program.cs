5f8955912613d9fc6e930e6a19ca74d23d5eba87
Yana <leduschenkova6@gmail.com>
2022-09-17 18:10:50 +0300

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2. Ввод двух чисел и показать какое из них большее, а какое меньшее
+Console.Write("Введите первое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numB= Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numA);
+    Console.Write("min = ");
+    Console.WriteLine(numB);
+}
+else
+    Console.Write("max = ");
+    Console.WriteLine(numB);
+    Console.Write("min = ");
+    Console.WriteLine(numA);
+
