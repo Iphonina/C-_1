1de0ab66c362f3472da8d4e4c4f7a83d39270a77
Yana <leduschenkova6@gmail.com>
2022-09-17 18:09:57 +0300

    add Task

---
+++ Task/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая на вход принимает число и выдает квадрат этого числа (возводит в степень)
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine($"Результат {result}");
+
+if(number == result)
+{
+    Console.WriteLine($"Не может такого быть!");
+}
+else
+{
+   Console.WriteLine($"Да");
+}
+
+
+
+
