4e3f2176530356148c11ffb58a362dd1db6ba6b7
Yana <leduschenkova6@gmail.com>
2022-09-17 18:53:31 +0300

    add Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4. Ввести три числа и найти максимальное
+Console.Write("Введите первое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numB= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int numC= Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if((numA > numB) && (numA > numC))
+    {
+        max = numA;
+    }
+else if((numB > numA) && (numB > numC))
+    {
+        max = numB;
+    }
+else if((numC > numA) && (numC > numB))
+    {
+        max = numC;
+    }
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
